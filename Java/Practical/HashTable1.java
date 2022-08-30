c82aecdc42398bd3181f22f512c4d4220bfc7245
debanath <debanath26@gmail.com>
2022-08-30 10:06:17 +0530

    java

--- Java/Practical/HashTable1.java
+++ Java/Practical/HashTable1.java
@@ -1,11 +1,8 @@
-
-// 4.	Create a program to set view of Keys from Java Hashtable.
 import java.util.*; 
-@SuppressWarnings("unchecked")
+@SuppressWarnings ("unchecked")
 class HashTable1 { 
     public static void main(String[] arg) 
     { 
-        // creating a hash table 
         Hashtable<Integer, String> h = new Hashtable<Integer, String>(); 
   
         Hashtable<Integer, String> h1 = new Hashtable<Integer, String>(); 
@@ -16,22 +13,16 @@ class HashTable1 {
 
 
        
-        // create a clone or shallow copy of hash table h 
-        
         h1 = (Hashtable<Integer, String>)h.clone(); 
   
-        // checking clone h1 
         System.out.println("values in clone: " + h1); 
   
-        // clear hash table h 
-        //h.clear(); 
 	    h.remove(2);
   
-        // checking hash table h 
         System.out.println("after clearing: " + h); 
         Enumeration<Integer> e = h.keys();
         while (e.hasMoreElements()) {
-         System.out.println(e.nextElement());
+        System.out.println(e.nextElement());
 }
     } 
 } 
